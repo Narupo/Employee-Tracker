c6f3611c15acba7fadf311936de20a3640f14ac2
Narupo <pouzumaki@gmail.com>
2025-03-05 22:06:41 -0600

    setup database connection, setup actions within index.ts

--- src/connection.ts
+++ src/connection.ts
@@ -1,27 +1,23 @@
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-// Import and require Pool (node-postgres)
-// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
-import pg from 'pg';
-const { Pool } = pg;
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: 'localhost',
-  database: process.env.DB_NAME,
-  port: 5432,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-const connectToDb = async () => {
-  try {
-    await pool.connect();
-    console.log('Connected to the database.');
-  } catch (err) {
-    console.error('Error connecting to database:', err);
-    process.exit(1);
-  }
-};
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
 
-export { pool, connectToDb };
+pool.on('error', (err) => {
+    console.error('Database connection error', err);
+});
+
+export default pool;
+
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import inquirer from 'inquirer';
+import pool from './connection.js';
+import { 
+    viewDepartments, 
+    viewRoles, 
+    viewEmployees, 
+    addDepartment, 
+    addRole, 
+    addEmployee, 
+    updateEmployeeRole } from './queries';
+
+async function mainMenu() {
+    const { action } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ]);
+
+    switch (action) {
+        case 'View all departments':
+            await viewDepartments();
+            break;
+        case 'View all roles':
+            await viewRoles();
+            break;
+        case 'View all employees':
+            await viewEmployees();
+            break;
+        case 'Add a department':
+            await addDepartment();
+            break;
+        case 'Add a role':
+            await addRole();
+            break;
+        case 'Add an employee':
+            await addEmployee();
+            break;
+        case 'Update an employee role':
+            await updateEmployeeRole();
+            break;
+        case 'Exit':
+            console.log('Goodbye!');
+            pool.end();
+            return;
+    }
+
+    mainMenu();
+}
+
+mainMenu();
